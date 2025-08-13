feb78d07c0e21decddd39c00840b3e43b5e91483
cornzange <cornzange@gmail.com>
2025-08-13 14:29:14 +0300

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import { init } from '@telegram-apps/sdk-react';
+try {
+    init();
+} catch (e) {
+    console.log(e)
+}
 createRoot(document.getElementById("root")!).render(<App />);
